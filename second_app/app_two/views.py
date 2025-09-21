6e6fbe573ab00a9437581e551a7ce1132a7be770
lei <lerahcaones18@gmail.com>
2025-09-21 21:44:28 +0800

    feature(app_two): complete app setup

--- second_app/app_two/views.py
+++ second_app/app_two/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {}
+    if request.method == "POST":
+        name = request.POST.get("name")
+        if name:
+            context["greeting"] = f"Hello, {name}! Welcome to my Website."
+    return render(request, 'two_app/home.html', context)
